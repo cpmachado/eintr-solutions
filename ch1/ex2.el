388f9d6ea65ccfda1f90e09071085a671a0de25a
cpmachado <cpmachado@protonmail.com>
2024-03-22 21:54:10 +0000

    First chapter

---
+++ ch1/ex2.el
@@ -0,0 +1,2 @@
+;; Exercise 1.11.2: Generate an error message by evaluating an appropriate symbol that is between parentheses.
+(fill-column)
---
+++ ch1/ex4.el
@@ -0,0 +1,2 @@
+;; Exercise 1.11.4: Write an expression that prints a message in the echo area when evaluated.
+(message "Hello World")
